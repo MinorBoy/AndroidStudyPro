a6d45fb9821b96de91fc47305d85dda625018902
MinorBoy <543763332@qq.com>
2017-11-07 19:47:39 +0800

    提交代码

--- AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
+++ AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
@@ -7,6 +7,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
     }
 }
