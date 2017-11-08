b40224de5e5858c5e5bd7aeafa7bf2a0001e0adc
MinorBoy <543763332@qq.com>
2017-11-08 18:57:53 +0800

    提交代码

--- AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
+++ AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.androidstudypro;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_layout);
+
+        Button btn1 = (Button) findViewById(R.id.btn1);
+        btn1.setOnClickListener(new View.OnClickListener(){
+//            @Override
+            public void onClick(View v){
+                Toast.makeText(MainActivity.this, "哈哈哈", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
