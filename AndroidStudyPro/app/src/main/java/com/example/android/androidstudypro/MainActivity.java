61da0c8d7e44b47c1ff2dc4ad07dfd93c80d38a9
MinorBoy <543763332@qq.com>
2017-11-12 12:41:19 +0800

    添加菜单

--- AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
+++ AndroidStudyPro/app/src/main/java/com/example/android/androidstudypro/MainActivity.java
@@ -2,11 +2,31 @@ package com.example.android.androidstudypro;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.setting:
+                Toast.makeText(MainActivity.this, "setting item", Toast.LENGTH_SHORT).show();
+            case R.id.about:
+                Toast.makeText(MainActivity.this, "about item", Toast.LENGTH_SHORT).show();
+            default:
+        }
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +41,7 @@ public class MainActivity extends AppCompatActivity {
                 Toast.makeText(MainActivity.this, "哈哈哈", Toast.LENGTH_SHORT).show();
             }
         });
+
+
     }
 }
